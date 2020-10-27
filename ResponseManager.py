768a896555908c2eafb81c1fd714ae7065ec7d8f
Mort09 <matthew.macmaster@routeique.com>
2020-10-26 22:00:35 -0600

    Grammar

--- ResponseManager.py
+++ ResponseManager.py
@@ -6,4 +6,4 @@ class Response:
         return """Hello, I am D&D bot. I exist to help with all your D&D needs and questions. My currrent knowledge base includes but is not limited to,classes, races, equipment, spells, monsters, mechanics and rules. I also manage my masters homebrews which I keep incase anyone is ever wondering what they are or if master ever forgets them. If you think my information is incorrect or outdated please message my creater to fix my ways."""
 
     def help():
-        return """ Here is a list of my commands and sub comadns to get you on your way to using D&DBot! """
+        return """ Here is a list of my commands and sub comamnds to get you on your way to using D&DBot! """
