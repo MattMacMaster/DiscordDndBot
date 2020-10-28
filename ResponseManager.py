01fc02a52e66c07279c8571cfc0afe235a836551
Mort09 <matthew.macmaster@routeique.com>
2020-10-27 22:17:37 -0600

    Added some formatiing and more msges

--- ResponseManager.py
+++ ResponseManager.py
@@ -2,8 +2,48 @@
 #This may include simply strings or api calls if necessary
 
 class Response:
+    ClassList = [
+            "$Classes/{ClassName}",
+            "$Classes/{ClassName}/Hit_Die",
+            "$Classes/{ClassName}/Proficiencies",
+            "$Classes/{ClassName}/Proficiency_choices",
+            "$Classes/{ClassName}/Saving_Throws",
+            "$Classes/{ClassName}/Sub_Classes",    
+            "$Classes/{ClassName}/Starting_Equipment",
+            "$Classes/{ClassName}/Class_Levels",
+            "$Classes/{ClassName}/Spellcasting",
+            "$Classes/{ClassName}/Spells",
+    ]
+
+    RaceList = [
+            "$Race/{RaceName}",
+            "$Race/{RaceName}/Speed",
+            "$Race/{RaceName}/Ability_Bonuses",
+            "$Race/{RaceName}/Starting_Proficiencies",
+            "$Race/{RaceName}/Languages",
+            "$Race/{RaceName}/Traits",
+            "$Race/{RaceName}/Sub_Classes",    
+            "$Race/{RaceName}/SubRaces",
+            "$Race/{RaceName}/Age",
+            "$Race/{RaceName}/Size",
+            "$Race/{RaceName}/Alignment",
+    ]
+
+    Character_Data = {
+        "Ability Scores": "$Character_info/ability-scores/con \n $Character_info/ability-scores/dex \n $Character_info/ability-scores/str \n $Character_info/ability-scores/int \n $Character_info/ability-scores/wis \n $Character_info/ability-scores/cha",
+        "Skills":"",
+        "Proficiencies":"",
+        "Languages":""
+    }
+
+
     def intro():
         return """Hello, I am D&D bot. I exist to help with all your D&D needs and questions. My currrent knowledge base includes but is not limited to,classes, races, equipment, spells, monsters, mechanics and rules. I also manage my masters homebrews which I keep incase anyone is ever wondering what they are or if master ever forgets them. If you think my information is incorrect or outdated please message my creater to fix my ways."""
 
     def help():
-        return """ Here is a list of my commands and sub comamnds to get you on your way to using D&DBot! """
+        return "Here is are the all the beginning level commands that are currently available, follow their respective helpers for more information!"
+
+    def character_def_help():
+        return "Here are the currently known commands for Character Information!"
+
+
--- Testbot.py
+++ Testbot.py
@@ -4,6 +4,8 @@ import random
 import ResponseManager
 import discord
 from discord.ext import commands
+import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,12 +29,38 @@ async def DnD(ctx):
 
 @client.command()
 async def help(ctx):
+    r =requests.get('https://www.dnd5eapi.co/api/ability-scores/cha')   
+    print(r.text)
+
     embed = discord.Embed(
-        title = 'Help',
+        title = 'Help - $help',
         description = ResponseManager.Response.help(),
         colour = discord.Colour.red()
     )
-    embed.add_field(name='Classes', value='test', inline=True)
+    embed.add_field(name='Character Info', value="$Character_info/help", inline=True)
+    embed.add_field(name='Classes', value='$Classes/help', inline=True)
+    embed.add_field(name='Races', value='$Races/help', inline=True)
+    embed.add_field(name='Equipment', value='$Equipment/help', inline=True)
+    embed.add_field(name='Spells', value='$Spells/help', inline=True)
+    embed.add_field(name='Monsters', value='$Monsters/help', inline=True)
+    embed.add_field(name='Mechanics', value='$Mechanics/help', inline=True)
+    embed.add_field(name='Rules', value='$Rules/help', inline=True)
+    embed.add_field(name='Homebrews', value='$Homebrews/help', inline=True)
+
+    await ctx.send(embed=embed)
+
+
+@client.command(name='Character_info/help')
+async def Character_info_help(ctx):
+    embed = discord.Embed(
+        title = 'Character Info Help',
+        description = ResponseManager.Response.character_def_help(),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='Ability Scores', value=str(ResponseManager.Response.Character_Data["Ability Scores"]), inline=True)
+    embed.add_field(name='Skills', value=ResponseManager.Response.Character_Data["Skills"], inline=True)
+    embed.add_field(name='Proficiencies', value=ResponseManager.Response.Character_Data["Proficiencies"], inline=True)
+    embed.add_field(name='Languages', value=ResponseManager.Response.Character_Data["Languages"], inline=True)
     await ctx.send(embed=embed)
 
            
