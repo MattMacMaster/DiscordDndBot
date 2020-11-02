ea434d2fc9d09103e342ca61c92e88e16092da6b
Mort09 <matthew.macmaster@routeique.com>
2020-11-01 20:39:32 -0700

    Added outliing fields

--- ResponseManager.py
+++ ResponseManager.py
@@ -2,53 +2,227 @@
 #This may include simply strings or api calls if necessary
 
 class Response:
-    ClassList = [
-            "$Classes/{ClassName}",
-            "$Classes/{ClassName}/Hit_Die",
-            "$Classes/{ClassName}/Proficiencies",
-            "$Classes/{ClassName}/Proficiency_choices",
-            "$Classes/{ClassName}/Saving_Throws",
-            "$Classes/{ClassName}/Sub_Classes",    
-            "$Classes/{ClassName}/Starting_Equipment",
-            "$Classes/{ClassName}/Class_Levels",
-            "$Classes/{ClassName}/Spellcasting",
-            "$Classes/{ClassName}/Spells",
-    ]
 
-    RaceList = [
-            "$Race/{RaceName}",
-            "$Race/{RaceName}/Speed",
-            "$Race/{RaceName}/Ability_Bonuses",
-            "$Race/{RaceName}/Starting_Proficiencies",
-            "$Race/{RaceName}/Languages",
-            "$Race/{RaceName}/Traits",
-            "$Race/{RaceName}/Sub_Classes",    
-            "$Race/{RaceName}/SubRaces",
-            "$Race/{RaceName}/Age",
-            "$Race/{RaceName}/Size",
-            "$Race/{RaceName}/Alignment",
-    ]
     Race_Data = {
-
+        "General": """
+        $Race {RaceName}
+        """,
+        "Specific": """
+        $Race/Speed {RaceName},
+          $Race/Ability_Bonuses {RaceName},
+           $Race/Starting_Proficiencies {RaceName},
+            $Race/Languages {RaceName},
+             $Race/Traits {RaceName},
+              $Race/Sub_Classes {RaceName},
+               $Race/SubRaces {RaceName},
+                $Race/Age {RaceName},
+                 $Race/Size {RaceName},
+                  $Race/Alignment {RaceName}
+                  """
     }
 
+    Monster_Data = {
+        "General": """
+        $Monster {MonsterName}
+        """,
+        "Main": 
+        """
+        $Monster/Speed {MonsterName},
+        $Monster/Type {MonsterName},
+        $Monster/Subtype {MonsterName},
+        $Monster/Armor_Class {MonsterName},
+        $Monster/Hit_Points {MonsterName},
+        $Monster/Hit_Dice {MonsterName},
+        $Monster/Ability_Scores {MonsterName},
+        $Monster/Strength {MonsterName},
+        $Monster/Dexterity {MonsterName},
+        $Monster/Constitution {MonsterName},
+        $Monster/Intelligence	 {MonsterName},
+        $Monster/Wisdom {MonsterName},
+        $Monster/Charisma {MonsterName},
+    
+        """,
+        "Specific": 
+        """ 
+        $Monster/Proficiencies {MonsterName},
+        $Monster/Size {MonsterName},
+        $Monster/Alignment {MonsterName}
+        $Monster/Damage_Vulnerabilities {MonsterName},
+        $Monster/Damage_Immunities {MonsterName},
+        $Monster/Damage_Resistances {MonsterName},
+        $Monster/Condition_Immunities {MonsterName},
+        $Monster/Senses {MonsterName},
+        $Monster/Languages {MonsterName},
+        $Monster/Legendary_Actions {MonsterName},
+        $Monster/Special_Abilities {MonsterName},
+        $Monster/Challenge_Rating {MonsterName},
+        """
+    }
+    Class_Data = {
+        "General": """
+        $Class {ClassName}
+        """,
+        "Main": 
+        """
+        $Class/Hit_Die {ClassName},
+        $Class/Proficiencies {ClassName},
+        $Class/Spells {ClassName},
+        $Class/Class_Levels {ClassName},
+        $Class/Proficiency_Choices {ClassName},
+        $Class/Saving_Throws {ClassName},
+        $Class/Starting_Equipment {ClassName},
+        $Class/SubClasses {ClassName},
+        """
+    }
+    Equipment_Data = {
+        "General": 
+        """
+        $Equipment {Name}
+        """,
+
+
+        "Armor": """ 
+        $Armor {name},
+        $Armor/Equipment_Category {name},
+        $Armor/Armor_Category {name},
+        $Armor/Armor_Class {name},
+        $Armor/Str_Minimum {name},
+        $Armor/Stealth_Disadvantage {name},
+        $Armor/Weight {name},
+        $Armor/Cost {name},
+         """,
+
+
+        "Weapon": 
+        """ 
+        $Weapon {name},
+        $Weapon/Equipment_Category {name},
+        $Weapon/Weapon_Category {name},
+        $Weapon/Weapon_Range {name},
+        $Weapon/Category_Range {name},
+        $Weapon/Range {name},
+        $Weapon/Cost {name},
+        $Weapon/Damage {name},
+        $Weapon/Weight {name},
+        $Weapon/Properties {name},
+        """,
+        "Magic Items": 
+        """ 
+        $Magic_Item {name},
+        $Magic_Item/Equipment_Category {name},
+        $Magic_Item/Desc {name},
+        """,
+        "Adventuring Gear": 
+        """
+        $Adv_Gear {name},
+        $Adv_Gear/Equipment_Category {name},
+        $Adv_Gear/Gear_Category {name},
+        $Adv_Gear/Cost {name},
+        $Adv_Gear/Weight {name},
+        """,
+        "Equipment Packs": 
+        """ 
+        $Equip_Pack {name},
+        $Equip_Pack/Equipment_Category {name},
+        $Equip_Pack/Gear_Category {name},
+        $Equip_Pack/Cost {name},
+        $Equip_Pack/Contents {name},
+        """,
+        "Weapon Properties": "SOON"
+ 
+    }
+    Spell_Data = {
+        "General":
+        """
+        $Spell {Spellname}
+        $Spell/School {Schoolname}
+        $Spell/Level {Level}
+        """,
+    
+        "Main": 
+        """ 
+         $Spell/Desc {Spellname},
+         $Spell/Range {Spellname},
+         $Spell/Higher_Level {Spellname},
+         $Spell/Components {Spellname},
+         $Spell/Material {Spellname},
+         $Spell/Ritual {Spellname},
+         $Spell/Duration {Spellname},
+         $Spell/Concentration {Spellname},
+         $Spell/Cast_Time {Spellname},
+         $Spell/Level {Spellname},
+         $Spell/Attack_Type {Spellname},
+         $Spell/Damage {Spellname},
+         $Spell/School {Spellname},
+         $Spell/Classes {Spellname},
+         $Spell/Subclasses {Spellname},
+        """
+    }
+    Mechanic_Data = {
+        "General":
+        """
+        $Mechanics/Conditions {name},
+        $Mechanics/Damage_Types {name},
+        $Mechanics/Schools {name},
+        """
+    }
+    Rules_Data = {
+        "General":
+        """
+        Soon
+        """
+    }
+    
 
     #Please god find a better way to format this in the embed tool
     Character_Data = {
-        "Ability Scores": "$Character_info/ability-scores/con \n $Character_info/ability-scores/dex \n $Character_info/ability-scores/str \n $Character_info/ability-scores/int \n $Character_info/ability-scores/wis \n $Character_info/ability-scores/cha",
-        "Skills":"Ex:($Character_info/skills/Arcana) \n $Character_info/skills/{name} \n $Character_info/skills/{name}/ability-score \n $Character_info/skills/{name}/desc",
-        "Proficiencies":"Ex:($Character_info/proficiencies/medium-armor) \n $Character_info/proficiencies/{name} \n $Character_info/proficiencies/{name}/classes \n $Character_info/proficiencies/{name}/races",
-        "Languages":"Ex:($Character_info/languanges/common) \n $Character_info/languanges/{name} \n $Character_info/languanges/{name}/type \n $Character_info/languanges/{name}/speakers"
+        "Ability Scores": """
+        Ex: ($Character_info/ability-scores con),
+        $Character_info/ability-scores {name},
+         $Character_info/ability-scores/skills {name}
+        """,
+        "Skills":
+        """
+        Ex: ($Character_info/skills/ Arcana),
+        $Character_info/skills {name},
+        $Character_info/skills/ability-score {name},
+        $Character_info/skills/desc {name},
+           """,
+        "Proficiencies":
+        """
+        Ex: ($Character_info/proficiencies medium-armor),
+        $Character_info/proficiencies {name},
+        $Character_info/proficiencies/classes {name},
+        $Character_info/proficiencies/races {name},
+           """,
+        "Languages":
+        """
+        Ex: ($Character_info/languanges common),
+         $Character_info/languanges {name},
+         $Character_info/languanges/type {name} ,
+         $Character_info/languanges/speakers {name},
+        """
     }
 
-
+    @staticmethod
     def intro():
-        return """Hello, I am D&D bot. I exist to help with all your D&D needs and questions. My currrent knowledge base includes but is not limited to,classes, races, equipment, spells, monsters, mechanics and rules. I also manage my masters homebrews which I keep incase anyone is ever wondering what they are or if master ever forgets them. If you think my information is incorrect or outdated please message my creater to fix my ways."""
-
+        return """
+        Hello,
+         I am D&D bot.
+          I exist to help with all your D&D needs and questions.
+           My currrent knowledge base includes but is not limited to,
+           classes, races, equipment, spells, monsters, mechanics and rules.
+            I also manage my masters homebrews which I keep incase anyone is ever
+             wondering what they are or if master ever forgets them.
+              If you think my information is incorrect or outdated please message
+               my creater to fix my ways.
+               """
+    @staticmethod
     def help():
-        return "Here is are the all the beginning level commands that are currently available, follow their respective helpers for more information!"
-
-    def character_def_help():
-        return "Here are the currently known commands for Character Information!"
+        return """Here is are the all the beginning level commands that are currently available,
+         follow their respective helpers for more information!"""
+    @staticmethod
+    def character_def_help(value):
+        return 'Here are the currently known commands for {} Information!'.format(value)
 
 
--- Testbot.py
+++ Testbot.py
@@ -15,6 +15,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = commands.Bot(command_prefix='$')
 client.remove_command('help')
 
+    #r =requests.get('https://www.dnd5eapi.co/api/ability-scores/cha')   
+    #print(r.text)
+
 @client.event
 async def on_ready():
     print('Bot is connected and working.')
@@ -30,8 +33,6 @@ async def DnD(ctx):
 
 @client.command()
 async def help(ctx):
-    r =requests.get('https://www.dnd5eapi.co/api/ability-scores/cha')   
-    print(r.text)
 
     embed = discord.Embed(
         title = 'Help - $help',
@@ -42,7 +43,7 @@ async def help(ctx):
     embed.add_field(name='Classes', value='$Classes/help', inline=True)
     embed.add_field(name='Races', value='$Races/help', inline=True)
     embed.add_field(name='Equipment', value='$Equipment/help', inline=True)
-    embed.add_field(name='Spells', value='$Spells/help', inline=True)
+    embed.add_field(name='Spells', value='$Spell/help', inline=True)
     embed.add_field(name='Monsters', value='$Monsters/help', inline=True)
     embed.add_field(name='Mechanics', value='$Mechanics/help', inline=True)
     embed.add_field(name='Rules', value='$Rules/help', inline=True)
@@ -56,8 +57,8 @@ async def help(ctx):
 @client.command(name='Character_info/help')
 async def Character_info_help(ctx):
     embed = discord.Embed(
-        title = 'Character Info Help',
-        description = ResponseManager.Response.character_def_help(),
+        title = 'Character Info Help - $Character_info/help',
+        description = ResponseManager.Response.character_def_help('Character Info'),
         colour = discord.Colour.red()
     )
     embed.add_field(name='Ability Scores', value=str(ResponseManager.Response.Character_Data["Ability Scores"]).strip('[]'), inline=False)
@@ -73,17 +74,98 @@ async def Character_info_help(ctx):
 @client.command(name='Races/help')
 async def Race_help(ctx):
     embed = discord.Embed(
-        title = 'Character Info Help',
-        description = ResponseManager.Response.character_def_help(),
+        title = 'Race Info Help - $Races/help',
+        description = ResponseManager.Response.character_def_help('Race'),
         colour = discord.Colour.red()
     )
-    embed.add_field(name='Ability Scores', value='', inline=False)
-    embed.add_field(name='Skills', value='', inline=False)
-    embed.add_field(name='Proficiencies', value='', inline=False)
-    embed.add_field(name='Languages', value='', inline=False)
+    embed.add_field(name='General', value=ResponseManager.Response.Race_Data["General"], inline=False)
+    embed.add_field(name='Specific', value=ResponseManager.Response.Race_Data["Specific"], inline=False)
     embed.timestamp = datetime.utcnow()
     embed.set_footer(text='MattMaster Bots: Dnd')
 
+    await ctx.send(embed=embed)
+
+@client.command(name='Monsters/help')
+async def Monster_help(ctx):
+    embed = discord.Embed(
+        title = 'Monster Info Help - $Monsters/help',
+        description = ResponseManager.Response.character_def_help('Monster'),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='General', value=ResponseManager.Response.Monster_Data["General"], inline=False)
+    embed.add_field(name='Main', value=ResponseManager.Response.Monster_Data["Main"], inline=False)
+    embed.add_field(name='Specific', value=ResponseManager.Response.Monster_Data["Specific"], inline=False)
+    embed.timestamp = datetime.utcnow()
+    embed.set_footer(text='MattMaster Bots: Dnd')
+
+    await ctx.send(embed=embed)
+
+@client.command(name='Classes/help')
+async def Monster_help(ctx):
+    embed = discord.Embed(
+        title = 'Classes Info Help - $Classes/help',
+        description = ResponseManager.Response.character_def_help('Class'),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='General', value=ResponseManager.Response.Class_Data["General"], inline=False)
+    embed.add_field(name='Main', value=ResponseManager.Response.Class_Data["Main"], inline=False)
+    embed.timestamp = datetime.utcnow()
+    embed.set_footer(text='MattMaster Bots: Dnd')
+    await ctx.send(embed=embed)
+
+@client.command(name='Equipment/help')
+async def Equipment_help(ctx):
+    embed = discord.Embed(
+        title = 'Equipment Info Help - $Equipment/help',
+        description = ResponseManager.Response.character_def_help('Equipment'),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='General', value=ResponseManager.Response.Equipment_Data["General"], inline=False)
+    embed.add_field(name='Armor', value=ResponseManager.Response.Equipment_Data["Armor"], inline=False)
+    embed.add_field(name='Weapon', value=ResponseManager.Response.Equipment_Data["Weapon"], inline=False)
+    embed.add_field(name='Magic Items', value=ResponseManager.Response.Equipment_Data["Magic Items"], inline=False)
+    embed.add_field(name='Adventuring Gear', value=ResponseManager.Response.Equipment_Data["Adventuring Gear"], inline=False)
+    embed.add_field(name='Equipment Packs', value=ResponseManager.Response.Equipment_Data["Equipment Packs"], inline=False)
+    embed.add_field(name='Weapon Properties', value=ResponseManager.Response.Equipment_Data["Weapon Properties"], inline=False)
+    embed.timestamp = datetime.utcnow()
+    embed.set_footer(text='MattMaster Bots: Dnd')
+    await ctx.send(embed=embed)
+
+@client.command(name='Spell/help')
+async def Spell_help(ctx):
+    embed = discord.Embed(
+        title = 'Spell Info Help - $Spell/help',
+        description = ResponseManager.Response.character_def_help('Spell'),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='General', value=ResponseManager.Response.Spell_Data["General"], inline=False)
+    embed.add_field(name='Main', value=ResponseManager.Response.Spell_Data["Main"], inline=False)
+    embed.timestamp = datetime.utcnow()
+    embed.set_footer(text='MattMaster Bots: Dnd')
+    await ctx.send(embed=embed)
+
+@client.command(name='Rules/help')
+async def Rules_help(ctx):
+    embed = discord.Embed(
+        title = 'Rules Info Help - $Rules/help',
+        description = ResponseManager.Response.character_def_help('Rule'),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='General', value=ResponseManager.Response.Rules_Data["General"], inline=False)
+    embed.timestamp = datetime.utcnow()
+    embed.set_footer(text='MattMaster Bots: Dnd')
+    await ctx.send(embed=embed)
+
+@client.command(name='Mechanics/help')
+async def Rules_help(ctx):
+    embed = discord.Embed(
+        title = 'Mechanics Info Help - $Mechanics/help',
+        description = ResponseManager.Response.character_def_help('Mechanics'),
+        colour = discord.Colour.red()
+    )
+    embed.add_field(name='General', value=ResponseManager.Response.Mechanic_Data["General"], inline=False)
+    embed.timestamp = datetime.utcnow()
+    embed.set_footer(text='MattMaster Bots: Dnd')
     await ctx.send(embed=embed)
 
            
